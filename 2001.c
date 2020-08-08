91fd04f14904eb520967fd92b029ab98eafcf46a
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-08 23:57:23 +0900

    2001

---
+++ 2001.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int pasta, juice, i, pmin, jmin;
+	pmin = jmin = 2100000000;
+	for(i = 0;i < 3;i++)
+	{
+		scanf("%d", &pasta);
+		if(pmin > pasta) pmin = pasta;
+	}
+	for(i = 0;i < 2;i++)
+	{
+		scanf("%d", &juice);
+		if(jmin > juice) jmin = juice;
+	}
+	printf("%d %d\n", pmin, jmin);
+	printf("%.1lf", (double)(pmin + jmin) * 1.1);
+	return 0;
+}
