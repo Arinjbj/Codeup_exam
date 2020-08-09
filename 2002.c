cbfdf87502ed304b9f661499d31a25233f98a4e5
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-09 21:59:23 +0900

    2002

---
+++ 2002.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int solve(int p, int k, char c);
+
+int main()
+{
+	int i, k;
+	char str[32];
+	scanf("%d\n%s", &k, str);
+	for(i = 0;i < strlen(str);i++)
+	{
+		printf("%c", solve(i + 1, k, str[i]));
+	}
+	return 0;
+}
+
+int solve(int p, int k, char c)
+{
+	c -= 3 * p + k;
+	while(c < 65)
+	{
+		c += 26;
+	}
+	return c;
+}
