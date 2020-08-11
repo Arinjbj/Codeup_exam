1e46a7f5d5927aec4f2d8667fafd25e1d6ac133e
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-12 05:05:05 +0900

    2004

---
+++ 2004.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int bx, by, ox, oy, i, j, sign = 0;
+	scanf("%d %d", &bx, &by);
+	scanf("%d %d", &ox, &oy);
+	
+	for(i = 0;i < (bx * ox);i++)
+	{
+		if(i % ox == 0) sign ^= 0b10;
+		for(j = 0;j < (by * oy);j++)
+		{
+			if(j == 0) sign = (sign & 0b10) + (sign >> 1);
+			if(j % oy == 0) sign ^= 1;
+			if((sign & 1) == 0) printf("X");
+			else printf(".");
+		}
+		printf("\n");
+	}
+	return 0;
+}
