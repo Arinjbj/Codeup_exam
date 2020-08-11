4ae977f88510178fbb8ef93e739fb14c4ccc873d
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-11 16:02:18 +0900

    2003

---
+++ 2003.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char icon[3][3];
+	int size, i, j;
+	strncpy(icon, "*x* xx* *", 9);
+	scanf("%d", &size);
+	for(i = 0;i < (3 * size);i++)
+	{
+		for(j = 0;j < (3 * size);j++)
+		{
+			printf("%c", icon[i / size][j / size]);
+			printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
