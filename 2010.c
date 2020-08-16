5a63d3073807745fe2e4d7e65f3543288b88786d
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-17 04:49:30 +0900

    Update 2010

--- 2010.c
+++ 2010.c
@@ -3,8 +3,14 @@
 
 int main()
 {
-	double b, n;
-	scanf("%lf %lf", &b, &n);
-	printf("%d", (int)(pow(b, 1 / n) + 0.5));
+	int b, n, i, temp1, temp2 = 1000001;
+	scanf("%d %d", &b, &n);
+	for(i = 1;;i++)
+	{
+		temp1 = abs(b - pow(i, n));
+		if(temp1 < temp2) temp2 = temp1;
+		else break;
+	}
+	printf("%d", --i);
 	return 0;
 }
