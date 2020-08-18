3eaff4557f73f9eaa9f55910901efe02aed467e4
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-18 23:50:31 +0900

    Update 2010.c

--- 2010.c
+++ 2010.c
@@ -1,15 +1,17 @@
-﻿#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int main()
 {
-	int b, n, i, temp1, temp2 = 1000001;
+	int b, n, i = 1, temp1, temp2 = 1000001;
 	scanf("%d %d", &b, &n);
-	for(i = 1;;i++)
+	while(1)
 	{
 		temp1 = abs(b - pow(i, n));
 		if(temp1 < temp2) temp2 = temp1;
 		else break;
+		i++;
 	}
 	printf("%d", --i);
 	return 0;
