f729ffd0eaa02e5e1de92aac97719f07aabfd153
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-18 14:07:23 +0900

    2012

---
+++ 2012.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a, b, i, one;
+	char str[8];
+	scanf("%d %d", &a, &b);
+	
+	for(one = 0;a <= b;a++)
+	{
+		sprintf(str, "%d", a);
+		for(i = 0;str[i];i++)
+		{
+			if(str[i] == '1') one++;
+		}
+	}
+	printf("%d", one);
+	return 0;
+}
