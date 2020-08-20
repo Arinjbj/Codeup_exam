2052f4e55b54f6c557efcbf64f04ebfe7c16178d
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-20 23:17:28 +0900

    2014

---
+++ 2014.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int star, tri, squa, tf = 0;
+	star = 1;
+	for(tri = 0;tri < 10;tri++)
+	{
+		for(squa = 0;squa < 10;squa++)
+		{
+			if((star * 100 + tri * 10 + squa) - (star * 10 + tri) == squa * 11)
+			{
+				tf = 1;
+				break;
+			}
+		}
+		if(tf != 0) break;
+	}
+	printf("%d%d%d-%d%d=%d%d", star, tri, squa, star, tri, squa, squa);
+	return 0;
+}
