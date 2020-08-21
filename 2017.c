4e6c4c8342c38b8a8b543c1eda9b3e5ff012c3b9
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-21 17:28:08 +0900

    2017

---
+++ 2017.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int n, k, i, temp;
+	char result[32];
+	scanf("%d %d", &n, &k);
+	
+	for(i = 0;n != 0;i++)
+	{
+		temp = n % k;
+		n /= k;
+		if(temp < 10) result[i] = temp + 48;
+		else result[i] = temp + 55;
+	}
+	for(i = strlen(result) - 1;i >= 0;i--)
+	{
+		printf("%c", result[i]);
+	}
+	return 0;
+}
