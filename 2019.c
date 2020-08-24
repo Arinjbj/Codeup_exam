9b3052fa71c60c2a35a11280cbb604e14c630599
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-25 03:26:39 +0900

    Update 2019.c

--- 2019.c
+++ 2019.c
@@ -1,14 +1,16 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <math.h>
 
 int main()
 {
-	double a, b, c, i;
+	double a, b, c, i, result;
 	scanf("%lf %lf %lf", &a, &b, &c);
 	i = (b * b) - (4 * a * c);
 	if(i == 0)
 	{
-		printf("%.2f", -b / (2 * a * c));
+		result = -b / (2 * a);
+		if(result == -0) result = 0;
+		printf("%.2f", result);
 	}
 	else if(i > 0)
 	{
@@ -18,8 +20,8 @@ int main()
 	else
 	{
 		i = -i;
-		printf("%.2f+%.2fi\n", -b / (2 * a), sqrt(i) / (2 * a));
-		printf("%.2f-%.2fi\n", -b / (2 * a), sqrt(i) / (2 * a));
+		printf("%.2f+%.2fi\n", -b / (2 * a), fabs(sqrt(i) / (2 * a)));
+		printf("%.2f-%.2fi\n", -b / (2 * a), fabs(sqrt(i) / (2 * a)));
 	}
 	return 0;
 }
