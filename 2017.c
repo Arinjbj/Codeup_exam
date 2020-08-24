da57d2a4544c943183e035f1baba0da6a60250ba
Arinjbj <67112867+Arinjbj@users.noreply.github.com>
2020-08-25 02:25:29 +0900

    Update 2017.c

--- 2017.c
+++ 2017.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 
 int main()
@@ -7,6 +7,12 @@ int main()
 	char result[32];
 	scanf("%d %d", &n, &k);
 	
+	if(n == 0)
+	{
+	    printf("0");
+	    return 0;
+	}
+	
 	for(i = 0;n != 0;i++)
 	{
 		temp = n % k;
@@ -14,6 +20,7 @@ int main()
 		if(temp < 10) result[i] = temp + 48;
 		else result[i] = temp + 55;
 	}
+	result[i] = NULL;
 	for(i = strlen(result) - 1;i >= 0;i--)
 	{
 		printf("%c", result[i]);
