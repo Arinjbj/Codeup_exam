0da01456df6fb5cb39c1e2a8c30f5e40dcf6cb51
ArinJBJ <qwert1424@naver.com>
2020-09-03 23:55:24 +0900

    2029

---
+++ 2029.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr[1000] = {1, 1};
+	int result[1000] = {1, 1};
+	int m, n, aloc, rloc;
+
+	scanf("%d %d", &m, &n);
+	n--;
+
+	for(int i = 1; i < m; i++)
+	{
+		for(int j = 0; result[j] != 0; j++) arr[j] = result[j];
+		aloc = rloc = 0;
+		result[rloc] = arr[aloc];
+		aloc = rloc = 1;
+		while(arr[aloc] != 0)
+		{
+			result[rloc] = arr[aloc - 1] + arr[aloc];
+			rloc++;
+			result[rloc] = arr[aloc];
+			aloc++;
+			rloc++;
+		}
+	}
+
+	printf("%d", result[n]);
+	return 0;
+}
