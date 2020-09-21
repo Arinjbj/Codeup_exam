fc59f7398f5a61d8d26e3a7e7a727f573d0e0a0a
Arinjbj <qwert1424@naver.com>
2020-09-21 22:44:08 +0900

    2045

--- 2045.c
+++ 2045.c
@@ -4,88 +4,36 @@
 int main()
 {
 	int islandcnt, bridgecnt;
-	int bridge[101][101] = {}, bridgetemp[101][101];
-	int islandcheck[101] = {};
-	int temp1, temp2, hasfindbridge, breakedbridge = 0;
-	int test, result;
-
+	int temp1, temp2;
+	int islands[101] = {};
+	int isodd = 0, result = 1;
 	scanf("%d %d", &islandcnt, &bridgecnt);
 	for (int i = 1; i <= bridgecnt; ++i)
 	{
 		scanf("%d %d", &temp1, &temp2);
-		if (temp1 < temp2)
-		{
-			++bridge[temp1][temp2];
-		}
-		else
-		{
-			++bridge[temp2][temp1];
-		}
+		++islands[temp1];
+		++islands[temp2];
 	}
-	memcpy(bridgetemp, bridge, sizeof(bridge));
-	for (test = 1; test < islandcnt; ++test)
+	for (int i = 1; i <= islandcnt; ++i)
 	{
-		if (test != 1)
+		if (islands[i] == 0 || isodd >= 3)
 		{
-			memcpy(bridge, bridgetemp, sizeof(bridgetemp));
-		}
-		else
-		{
-			if (islandcnt > bridgecnt)
-			{
-				result = 0;
-				break;
-			}
-		}
-		breakedbridge = 0;
-		memset(islandcheck, 0, islandcnt + 1);
-		while (1)
-		{
-			++islandcheck[test];
-			hasfindbridge = 0;
-			for (int i = 1; i <= islandcnt; ++i)
-			{
-				if (bridge[test][i] != 0)
-				{
-					hasfindbridge = 1;
-					--bridge[test][i];
-				}
-				else if (bridge[i][test] != 0)
-				{
-					hasfindbridge = 1;
-					--bridge[i][test];
-				}
-				if (hasfindbridge == 1)
-				{
-					++breakedbridge;
-					test = i;
-					break;
-				}
-			}
-			if (hasfindbridge == 0) break;
-		}
-		result = 1;
-		for (int i = 1; i <= islandcnt; ++i)
-		{
-			if (islandcheck[i] == 0)
-			{
-				result = 0;
-				break;
-			}
-		}
-		if (result != 0 && breakedbridge == bridgecnt)
-		{
-			result = 2;
+			result = 0;
 			break;
 		}
+		else if ((islands[i] & 1) == 1)
+		{
+			++isodd;
+		}
 	}
-	if (result == 2)
+	if (isodd == 1) result = 0;
+	if (result == 0)
 	{
-		printf("YES");
+		printf("NO");
 	}
 	else
 	{
-		printf("NO");
+		printf("YES");
 	}
 	return 0;
 }
